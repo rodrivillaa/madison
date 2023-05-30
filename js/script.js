e8efafd13feb6a85ab4cba983535079f6709d249
rodri <rodrivillaa23@gmail.com>
2023-05-30 05:02:54 -0300

    inicio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+    } else {
+    menu.style.display = 'block';
+    }
+}
+
+var carouselSlide = document.querySelector('.carousel-slide');
+    var images = carouselSlide.getElementsByTagName('img');
+    var imageIndex = 0;
+
+    function carousel() {
+      imageIndex = (imageIndex + 1) % images.length;
+      carouselSlide.style.transform = 'translateX(-' + (imageIndex * 100) + '%)';
+    }
+
+    setInterval(carousel, 3000); // Cambiar de imagen cada 3 segundos
